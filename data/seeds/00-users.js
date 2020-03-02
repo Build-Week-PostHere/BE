24ffdd5a526107ad08825003e35fc905142a29d5
Nicholas Sandomeno <nicholassandomeno@computer-2.home>
2020-03-02 11:14:47 -0500

    seeds modified for production.

--- data/seeds/00-users.js
+++ data/seeds/00-users.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users').truncate()
-    .then(function () {
+
+  // return knex('users').truncate()
+  //   .then(function () {
+
       // Inserts seed entries
       return knex('users').insert([
         {id: 1, username: 'Nick', password: 'pass'},
         {id: 2, username: 'Daniel', password: 'pass'},
         {id:3, username: 'Josiah', password: 'pass'}
       ]);
-    });
+    // });
 };
